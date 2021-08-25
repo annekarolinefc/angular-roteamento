8cbbf9b84803eb2403737add8d3d429efbe361cb
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-08-25 15:40:51 -0300

    Criando Rotas com o Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { Page2Component } from './page2/page2.component';
+import { Page1Component } from './page1/page1.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //INSERINDO AS PÁGINAS
+  {path: 'page1', component: Page1Component},
+  {path: 'page2', component: Page2Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
